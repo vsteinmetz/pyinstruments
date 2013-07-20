7d0329d378873e63dd0685e398a66d4873f98969
Leo <neuhaus@spectro.jussieu.fr>
2013-07-20 17:35:38 +0200

    TDS2024b scope driver
    rudimentary scope driver and bugfix

--- pyinstruments/drivers/visa/__init__.py
+++ pyinstruments/drivers/visa/__init__.py
@@ -2,4 +2,5 @@
 Visa communication using the module "pyVisa"
 """
 
-from visa_driver import VisaDriver
+from pyinstruments.drivers.visa.visa_driver import VisaDriver
+from pyinstruments.drivers.visa.tds2024b import TDS2024B
