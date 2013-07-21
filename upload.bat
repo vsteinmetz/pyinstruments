a1b3526faa05196d4cb6d9208c1f2bccbcea033b
Samuel Deléglise <samuel.deleglise@gmail.com>
2013-07-21 10:50:30 +0200

    upload.bat changed somehow

--- upload.bat
+++ upload.bat
@@ -1,6 +1,7 @@
+set HOME=C:\Users\Owner\
 cd C:\Users\Samuel\Documents\GitHub\pyinstruments
 
 C:\Python27\python.exe setup.py bdist_egg upload --identity="Samuel Deleglise" --sign --quiet
-C:\Python27\python.exe setup.py bdist_wininst --target-version=2.7 register upload --identity="Samuel Deleglise" --sign --quiet
-C:\Python27\python.exe setup.py sdist upload --identity="Samuel Deleglise" --sign
+C:\Python27\python.exe setup.py bdist_wininst register upload --identity="Samuel Deleglise" --sign --quiet
+C:\Python27\python.exe setup.py sdist upload --identity="Samuel Deleglise" --sign --quiet
 pause
