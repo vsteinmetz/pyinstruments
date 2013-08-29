491165e26eae7fc715f24bc4a89725d4be6f75ba
Samuel Deléglise <samuel.deleglise@gmail.com>
2013-08-29 19:18:08 +0200

    version incremented

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name = "pyinstruments",
     cmdclass={"install": installWithPost},
     scripts={'postinstallscript.py'},
-    version = "0.2.10",
+    version = "0.2.11",
     author = "Samuel Deleglise",
     author_email = "samuel.deleglise@gmail.com",
     description = ("""Control of data acquisition with remote instruments using 
